b0f7d64f7e996366cced832b1a1b6afb4d6334b5
Kayky Plombon Peres <85962125+KaykyPeres@users.noreply.github.com>
2024-11-27 01:50:20 -0300

    uptade no codigo

--- Modulo.vhdl
+++ Modulo.vhdl
@@ -6,7 +6,6 @@ END testepj;
 
 ARCHITECTURE behavior OF testepj IS 
 
-    -- Component Declaration for the Unit Under Test (UUT)
     COMPONENT modulopj
     PORT(
          clock : IN  std_logic;
@@ -15,81 +14,75 @@ ARCHITECTURE behavior OF testepj IS
         );
     END COMPONENT;
     
-   --Inputs
    signal clock : std_logic := '0';
    signal reset : std_logic := '0';
 
-   --Outputs
    signal output_internal: integer range 0 to 7;
 
 BEGIN
 
-    -- Instantiate the Unit Under Test (UUT)
     uut: modulopj PORT MAP (
           clock => clock,
           reset => reset,
           output => output_internal
         );
 
-    -- Stimulus process
     stim_proc: process
     begin		
-        -- Apply reset
+        
     reset <= '1';
     wait for 100 ns;
     reset <= '0';
     wait for 100 ns;
 		  
-	 clock <= '0';
-	 wait for 100 ns;
-	 clock <= '1';
-	 wait for 100 ns;
-	 clock <= '0';
-	 wait for 100 ns;
+    clock <= '0';
+    wait for 100 ns;
+    clock <= '1';
+    wait for 100 ns;
+    clock <= '0';
+    wait for 100 ns;
 		  
-	 clock <= '0';
+    clock <= '0';
     wait for 100 ns;
     clock <= '1';
     wait for 100 ns;
     clock <= '0';
     wait for 100 ns;
 	 
-     clock <= '0';
+    clock <= '0';
     wait for 100 ns;
     clock <= '1';
     wait for 100 ns;
     clock <= '0';
     wait for 100 ns;
 	 
-     clock <= '0';
+    clock <= '0';
     wait for 100 ns;
     clock <= '1';
     wait for 100 ns;
     clock <= '0';
     wait for 100 ns;
 	 
-	  clock <= '0';
+    clock <= '0';
     wait for 100 ns;
     clock <= '1';
     wait for 100 ns;
     clock <= '0';
     wait for 100 ns;
 	 
-	  clock <= '0';
+    clock <= '0';
     wait for 100 ns;
     clock <= '1';
     wait for 100 ns;
     clock <= '0';
     wait for 100 ns;
 	 
-	 clock <= '0';
+    clock <= '0';
     wait for 100 ns;
     clock <= '1';
     wait for 100 ns;
     clock <= '0';
     wait for 100 ns;
-	 
-	 
 	 
         wait;
     end process;
